355a620033338372d892489b2985f9de165a9349
Minsung KIM <ava979701@gmail.com>
2025-09-12 13:25:15 +0900

    Revert "docs: 불필요한 주석 제거 처리"
    This reverts commit 20d9dc8770a7db48062721ed5192170e13543080.

--- coordiapp/lib/main.dart
+++ coordiapp/lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-
+// 음
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget { 
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
